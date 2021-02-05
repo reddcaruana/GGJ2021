f4dbed385be1d536c0d883b40874dbe1a2ddb633
John Scerri <jscerri@vrtron.com>
2021-02-05 19:25:00 +0100

    Fish Data sample

---
+++ Assets/Scripts/Fish/FishData.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Assets.Scripts.Fish
+{
+	public enum FishType { Awrata, Vopa}
+
+	public readonly struct FishData
+	{
+		public readonly FishTypeData Type;
+	 	public readonly float MinWeight;
+	 	public readonly float MaxWeight;
+
+		public FishData(FishTypeData type, float minWeight, float maxWeight)
+		{
+			Type = type;
+			MinWeight = minWeight;
+			MaxWeight = maxWeight;
+		}
+	}
+
+	public readonly struct FishTypeData
+	{
+		public readonly FishType Type;
+		public readonly string NiceName;
+
+		public FishTypeData(FishType type, string niceName)
+		{
+			Type = type;
+			NiceName = niceName;
+		}
+	}
+}
---
+++ Assets/Scripts/Fish/FishWiki.cs
@@ -0,0 +1,8 @@
+﻿namespace Assets.Scripts.Fish
+{
+	public static class FishWiki
+	{
+		public static readonly FishData Awrata = new FishData(new FishTypeData(FishType.Awrata, "Awrata"), 1, 5);
+
+	}
+}
