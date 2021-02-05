cb35a1621286440d33d6aaf15b3b8c5b152e9569
reddcaruana <redd.caruana@gmail.com>
2021-02-05 20:30:50 +0100

    Basic Camera Movement

---
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts.Camera
+{
+    public class CameraControl : MonoBehaviour
+    {
+        float modifier = 0.5f;
+
+        bool isHolding = false;
+
+        void OnPress()
+        {
+            isHolding = true;
+        }
+
+        void OnRelease()
+        {
+            isHolding = false;
+        }
+
+        void OnMove(InputValue value)
+        {
+            if (isHolding)
+            {
+                Vector2 delta = value.Get<Vector2>();
+                transform.Translate(Vector3.up * delta.y * modifier);
+            }
+        }
+    }
+}
