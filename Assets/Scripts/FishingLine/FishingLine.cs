4db8dcaf3c0ac20d0497827501bbd352572e1db9
reddcaruana <redd.caruana@gmail.com>
2021-02-06 10:17:40 +0100

    Positioning and Stress
    Line positioning using references.
    Refactoring of code.

---
+++ Assets/Scripts/FishingLine/FishingLine.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.FishingLine
+{
+    public class FishingLine : MonoBehaviour
+    {
+        // The line renderer component.
+        private LineRenderer _lineRenderer;
+
+        // The number of segments.
+        private int _nPoints = 30;
+
+        // The point positions.
+        private Vector3[] _points;
+
+        // The anchor position.
+        private Vector3 _anchorPosition = Vector3.zero;
+
+        // The control position.
+        private Vector3 _controlPosition = Vector3.zero;
+
+        // The hook position.
+        private Vector3 _hookPosition = Vector3.zero;
+
+        [Header("Testing")]
+        [SerializeField]
+        private Transform _anchor = null;
+
+        [SerializeField]
+        private Transform _hook = null;
+
+        [SerializeField]
+        private Transform _control = null;
+
+        [SerializeField]
+        private bool _isTesting = false;
+
+        void Awake()
+        {
+            _lineRenderer = GetComponent<LineRenderer>();
+        }
+
+        void Update()
+        {
+            if (_isTesting)
+            {
+                _anchorPosition = _anchor.position;
+                _hookPosition = _hook.position;
+
+                _controlPosition = (_control == null) ? Vector3.Lerp(_anchorPosition, _hookPosition, 0.5f) : _control.position;
+
+                SetPositions(_anchorPosition, _controlPosition, _hookPosition);
+            }
+        }
+
+        /// <summary>
+        /// Links the line to an anchor.
+        /// </summary>
+        /// <param name="anchor">The anchor transform.</param>
+        public void SetAnchor(Transform anchor)
+        {
+            _anchor = anchor;
+            if (anchor != null) _anchorPosition = anchor.position;
+        }
+
+        /// <summary>
+        /// Anchors the line on a position.
+        /// </summary>
+        /// <param name="value">The position.</param>
+        public void SetAnchor(Vector3 value)
+        {
+            _anchor = null;
+            _anchorPosition = value;
+        }
+
+        /// <summary>
+        /// Links the control point.
+        /// </summary>
+        /// <param name="control">The control transform.</param>
+        public void SetControl(Transform control)
+        {
+            _control = control;
+            _controlPosition = control.position;
+        }
+
+        /// <summary>
+        /// Sets the control position.
+        /// </summary>
+        /// <param name="control">The position.</param>
+        public void SetControl(Vector3 value)
+        {
+            _control = null;
+            _controlPosition = value;
+        }
+
+        /// <summary>
+        /// Links the line to a hook.
+        /// </summary>
+        /// <param name="hook">The hook transform.</param>
+        public void SetHook(Transform hook)
+        {
+            _hook = hook;
+            _hookPosition = hook.position;
+        }
+
+        /// <summary>
+        /// Hooks the line on a position.
+        /// </summary>
+        /// <param name="value">The position.</param>
+        public void SetHook(Vector3 value)
+        {
+            _hook = null;
+            _hookPosition = value;
+        }
+
+        /// <summary>
+        /// Positions the line and all its points.
+        /// </summary>
+        /// <param name="start">The start position.</param>
+        /// <param name="control">The control position.</param>
+        /// <param name="end">The end position.</param>
+        public void SetPositions(Vector3 start, Vector3 control, Vector3 end)
+        {
+            _points = new Vector3[_nPoints];
+            
+            int maxPoint = _nPoints - 1;
+            for (int i = 0; i < _nPoints; i++)
+                _points[i] = GetPosition(start, control, end, (float) i / maxPoint);
+
+            _lineRenderer.positionCount = _nPoints;
+            _lineRenderer.SetPositions(_points);
+        }
+
+        /// <summary>
+        /// Gets the position of a point on a line.
+        /// </summary>
+        /// <param name="start">The start position.</param>
+        /// <param name="control">The control position.</param>
+        /// <param name="end">The end position.</param>
+        /// <param name="t">The normalized point value.</param>
+        Vector3 GetPosition(Vector3 start, Vector3 control, Vector3 end, float t)
+            => Vector3.Lerp(Vector3.Lerp(start, control, t), Vector3.Lerp(control, end, t), t);
+    }
+}
--- Assets/Scripts/FishingLine/FishingLineHealth.cs
+++ Assets/Scripts/FishingLine/FishingLineHealth.cs
@@ -8,69 +8,65 @@ namespace Assets.Scripts.FishingLine
     public class FishingLineHealth : MonoBehaviour
     {
         // The line renderer component.
-        private LineRenderer lineRenderer;
-
-        // The number of points.
-        private int nPoints = 30;
-
-        // The point positions.
-        private Vector3[] points;
+        private LineRenderer _lineRenderer;
 
         // The gradient blending value.
-        private float blending = 0.025f;
+        private float _blending = 0.025f;
 
         // The change of color position.
-        private float position = 1f;
+        private float _position = 1f;
+
+        // The current stress.
+        [SerializeField]
+        private float _stress = 0;
+
+        // The maximum stress.
+        private float _maxStress = 10;
 
         void Awake()
         {
-            lineRenderer = GetComponent<LineRenderer>();
-        }
-
-        void Start()
-        {
-            SetPositions(Vector3.zero, Vector3.up * 10);
+            _lineRenderer = GetComponent<LineRenderer>();
         }
 
         void Update()
         {
-            SetColor(Color.white, Color.red, position);
+            SetStress(_stress);
         }
 
-        Vector3 GetPosition(Vector3 start, Vector3 end, float x)
-        {
-            return x * Vector3.Lerp(start, end, x);
-        }
-
-        public void SetColor(Color start, Color end, float position)
+        /// <summary>
+        /// Sets the color of the fishing line.
+        /// </summary>
+        /// <param name="normal">The normal color.</param>
+        /// <param name="stress">The stress color.</param>
+        /// <param name="position">The normalized position.</param>
+        private void SetColor(Color normal, Color stress, float position)
         {
             Gradient gradient = new Gradient();
 
-            float minBlend = (position > 0 && position < 1) ? Mathf.Clamp(position - blending, 0f, 1f) : position;
-            float maxBlend = (position > 0 && position < 1) ? Mathf.Clamp(position + blending, 0f, 1f) : position;
+            float minBlend = (position > 0 && position < 1) ? Mathf.Clamp(position - _blending, 0f, 1f) : position;
+            float maxBlend = (position > 0 && position < 1) ? Mathf.Clamp(position + _blending, 0f, 1f) : position;
 
             gradient.SetKeys(
                 new GradientColorKey[] {
-                    new GradientColorKey(start, 0f),
-                    new GradientColorKey(start, minBlend),
-                    new GradientColorKey(end, maxBlend),
-                    new GradientColorKey(end, 1f)
+                    new GradientColorKey(normal, 0f),
+                    new GradientColorKey(normal, minBlend),
+                    new GradientColorKey(stress, maxBlend),
+                    new GradientColorKey(stress, 1f)
                 },
                 new GradientAlphaKey[] { new GradientAlphaKey(1, 0), new GradientAlphaKey(1, 1) }
             );
 
-            lineRenderer.colorGradient = gradient;
+            _lineRenderer.colorGradient = gradient;
         }
 
-        public void SetPositions(Vector3 start, Vector3 end)
+        /// <summary>
+        /// Sets the fishing line stress.
+        /// </summary>
+        /// <param name="value">The stress value.</param>
+        public void SetStress(float value)
         {
-            points = new Vector3[nPoints];
-            for (int i = 0; i < nPoints; i++)
-            {
-                points[i] = GetPosition(start, end, (float) i / nPoints);
-            }
-            lineRenderer.positionCount = nPoints;
-            lineRenderer.SetPositions(points);
+            _stress = value;
+            SetColor(Color.white, Color.red, (_maxStress - _stress) / _maxStress);
         }
     }
 }
